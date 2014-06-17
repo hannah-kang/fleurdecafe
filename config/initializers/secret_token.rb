523c9628179df4283ecb38326c483eee5cf2578a
MI55_H4NN4H <kang.hannah14@gmail.com>
2014-06-17 14:53:06 -0700

    what i have so far

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fleurdecafe::Application.config.secret_key_base = '563ea35e201317e848745381211cf7cdf0844faf826ecc0a383b914d7a46d7dac2a8b62a5bfc41b867e2a4c7b6745d8eeb613b88f81c144d9cd51ecc67048140'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fleurdecafe::Application.config.session_store :cookie_store, key: '_fleurdecafe_session'
